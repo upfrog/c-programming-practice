277b9fcc33ea38e7bae0adfcfe60097551e97fe6
Stephen Rout <srr@routhouse.org>
2024-03-07 21:54:59 -0500

    Complete 1_15

---
+++ ch_01/1_15.c
@@ -0,0 +1,29 @@
+/*
+Rewrite the temperature conversion program of Section 1.2 to use a function for conversion.
+*/
+#include <stdio.h>
+
+float fahr_to_celsius(float fahr);
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr <= upper)
+	{
+		celsius =  fahr_to_celsius(fahr);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
+
+float fahr_to_celsius(float fahr)
+{
+	return (5.0/9.0) * (fahr-32.0);
+}
--- ch_01/experiment.c
+++ ch_01/experiment.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
 
+int power(int m, int n);
+
 int main()
 {
-	int c, i, nwhite, nother;
-	int ndigit[10];
+	int i;
 
-	nwhite = nother = 0;
-
-	for (i = 0; i <10; i++)
-	{
-		ndigit[i] = 0;
-	}
-
-	while ((c = getchar()) != EOF)
-	{
-		if (c >= '0' && c <= '9')
-		{
-			++ndigit[c-'0'];
-		}
-		else if (c == ' ' || c == '\n' || c == '\t')
-		{
-			++nwhite;
-		}
-		else
-		{
-			++nother;
-		}
-	}
-
-	printf("digits =");
 	for (i = 0; i < 10; i++)
 	{
-		printf(" %d", ndigit[i]);
+		printf("%d %d %d\n", i, power(2,i), power(-3,i));
 	}
-	printf(", white space = %d, other = %d\n", nwhite, nother);
+
+	return 0;
 }
 
+int power(int base, int n)
+{
+	int i, p;
+
+	p = 1;
+	for (i =1; i <= n; ++i)
+	{
+		p = p * base;
+	}
+
+	return p;
+}
