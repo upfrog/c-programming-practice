c6edf5634bc8e8e5ef17d6610704ddb99e2cdb1b
Stephen <srr@routhouse.org>
2024-12-02 22:58:42 -0500

    Partially implement 03, need to refactor

--- 03/03.c
+++ 03/03.c
@@ -1,17 +1,23 @@
 /*
 NOT COMPLETED
 
-Excercise 3-3. Write a function expand(s1,s2) that expands shorthand notations like a-z in the string s1 into the equivalent complete list abc...xyz in s2. Allow for letters of either case and digits, and be prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange that a leading or trailing - is taken literall.
+Excercise 3-3. Write a function expand(s1,s2) that expands shorthand notations like a-z in 
+the string s1 into the equivalent complete list abc...xyz in s2. Allow for letters of either 
+case and digits, and be prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange that 
+a leading or trailing - is taken literally.
 */
 
 #include <stdio.h>
 
 #define MAXINPUTLEN 25
 #define MAXOUTPUTLEN 500
+#define TRUE 1
+#define FALSE 0
 
 void expand(char[], char[]);
 int advance(char[], int, char);	
-int write_sequence(char[], int, char);
+int is_sequence(char[], int, char);
+int write_sequence(char[], int, char, char);
 
 
 /*
@@ -22,6 +28,8 @@ To do that, we just advance until we hit the end of the sequence, but no further
 After that, we copy characters directly until we hit the start of the next sequence.
 
 
+
+switch case on set of symbolic constants, each of which refers to the start of a sequence
 */
 
 
@@ -32,42 +40,86 @@ int main()
 	char s1[MAXINPUTLEN];
 	char s2[MAXOUTPUTLEN];	
 	fgets(s1, MAXINPUTLEN, stdin);
-
+	expand(s1, s2);
+	printf("%s\n", s2);
 
 }
 
 
 
-void expands(char s1[], s2[])
+void expand(char s1[], char s2[])
 {
 	int i = 0;
 	int j = 0;
-
+	char seq_end = NULL;
 
 	while (s1[i] != '\0')
 	{
-		if (s1[i] == "a")
+		if (s1[i] == 'a')
 		{
-				
+			seq_end = 'z';
+			if (is_sequence(s1, i, seq_end))
+			{
+				write_sequence(s2, j, 'a', 'z');
+				++i;
+				j += 26;
+				i += 3;
+			}
+
+			
 		}
-		else if (s1[i] == "A")
+		else if (s1[i] == 'A')
 		{
-			//same, but for A-Z
+			seq_end = 'Z';
+			if (is_sequence(s1, i, seq_end))
+			{
+				
+				write_sequence(s2, j, 'A', 'Z');
+				++i;
+				j += 26;
+				i += 3;
+			}
 		}
 
-		else if (s1[i] == 0 || s1[i] == "0")
+		else if (s1[i] == 0 || s1[i] == '0')
 		{
-			//copy 0-9
+			seq_end = '9';
+			if (is_sequence(s1, i, seq_end))
+			{
+
+				write_sequence(s2, j, '0', '9');
+				++i;
+				j += 10;
+				i += 3;
+			}
 		}
 		else
 		{
+			s2[j] = s1[i];
 			//copy s1[] to s2
-
+			++i;
+			++j;
 		}
 	}
+}
 
 
-
+/*
+To be a valid sequence, a substring must start with the first character of the
+sequence, then be followed by a slash, then the final character of the sequence.
+*/
+int is_sequence(char s2[], int i, char seq_end)
+{
+	if (i < MAXOUTPUTLEN - 1
+	&& s2[i+1] == '-'
+	&& s2[i+2] == seq_end)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
 }
 
 
@@ -78,11 +130,21 @@ int advance(char s2[], int start, char target)
 		++start;
 	}
 
-	return start
+	return start;
 }
 
-	
-int write_sequence(char s2[], int start, char sequence_start)
+/*
+Fills a sequence of characters into s2, starting from some start index and character
+
+Assumes that the end of the sequence has an ASCII value greater or equal to the start.
+*/
+int write_sequence(char s2[], int start, char sequence_start, char sequence_end)
 {
-	
+	char cur_char = sequence_start;
+	while (cur_char <= sequence_end)
+	{
+		s2[start] = cur_char;
+		++start;
+		++cur_char;
+	}
 }
